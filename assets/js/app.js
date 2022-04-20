c1930775fa9d148f3fa20bf0b68f9a446c74b754
GabriPini <lele19977@gmail.com>
2022-04-20 13:05:01 +0200

    initial and final commit

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+const app = new Vue(
+	{
+		el : '#app',
+		data : {
+			message : 'I am Grogu!',
+			myClass: 'text-center ',
+			mySecondClass: 'fw-bold',
+			img: 'https://i0.wp.com/www.badtaste.it/tv/wp-content/uploads/sites/4/2021/02/grogu-hot-toys.jpg?fit=1200%2C600&quality=85&strip=all&ssl=1',
+			imgPlaceholder: 'Grogu image'
+		}
+	}
+);
